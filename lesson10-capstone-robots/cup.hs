1846b738afdfd92ca51060295054b9cb5d2a6826
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-03-15 16:00:52 +0000

    finished lesson 10

--- lesson10-capstone-robots/cup.hs
+++ lesson10-capstone-robots/cup.hs
@@ -4,6 +4,43 @@ coffeeCup = cup 12
 
 getOz aCup = aCup (\flOz -> flOz)
 
-drink aCup ozDrank = cup (flOz - ozDrank)
+drink aCup ozDrank = cup (max (flOz - ozDrank) 0)
   where
     flOz = getOz aCup
+
+isEmpty aCup = getOz aCup == 0
+
+robot (name, attack, hp) = \message -> message (name, attack, hp)
+
+killerRobot = robot ("Kill3r", 25, 200)
+
+name (n, _, _) = n
+
+getName aRobot = aRobot name
+
+attack (_, a, _) = a
+
+getAttack aRobot = aRobot attack
+
+hp (_, _, hp) = hp
+
+getHP aRobot = aRobot hp
+
+setName aRobot newName = aRobot (\(n, a, h) -> robot (newName, a, h))
+
+setAttack aRobot newAttack = aRobot (\(n, a, h) -> robot (n, newAttack, h))
+
+setHP aRobot newHP = aRobot (\(n, a, h) -> robot (n, a, newHP))
+
+printRobot aRobot = aRobot (\(n, a, h) -> n ++ " attack:" ++ (show a) ++ " hp:" ++ (show h))
+
+damage aRobot attackDamage = aRobot (\(n, a, h) -> robot (n, a, (h - attackDamage)))
+
+fight aRobot defender = damage defender attack
+  where
+    attack =
+      if getHP aRobot > 0
+        then getAttack aRobot
+        else 0
+
+gentleGiant = robot ("Mr. Friendly", 10, 300)
