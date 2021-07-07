b2927b261e32043773ecd1eb0033c2a91b65d213
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-07-07 10:26:26 +0100

    Finished Lesson 19

--- unit3/Lesson19.hs
+++ unit3/Lesson19.hs
@@ -1,6 +1,7 @@
 -- Lesson 19. The Maybe type: dealing with missing values --
 
 import Data.List
+import Data.Maybe
 import qualified Data.Map as Map
 
 data Organ = Heart | Brain | Kidney | Spleen deriving (Show, Eq)
@@ -51,3 +52,54 @@ cleanList = intercalate ", " organList
 numOrZero :: Maybe Int -> Int
 numOrZero Nothing = 0
 numOrZero (Just n) = n
+
+data Container = Vat Organ | Cooler Organ | Bag Organ
+
+instance Show Container where
+  show (Vat organ) = show organ ++ " in a vat"
+  show (Cooler organ) = show organ ++ " in a cooler"
+  show (Bag organ) = show organ ++ " in a bag"
+
+organToContainer :: Organ -> Container
+organToContainer Brain = Vat Brain
+organToContainer Heart = Cooler Heart
+organToContainer organ = Bag organ
+
+data Location = Lab | Kitchen | Bathroom deriving (Show)
+
+placeInLocation :: Container -> (Location, Container)
+placeInLocation (Vat a) = (Lab, Vat a)
+placeInLocation (Cooler a) = (Lab, Cooler a)
+placeInLocation (Bag a) = (Kitchen, Bag a)
+
+process :: Organ -> (Location, Container)
+process = placeInLocation . organToContainer
+
+report :: (Location, Container) -> String
+report (location, container) =
+  show container
+    ++ " in the "
+    ++ show location
+
+processAndReport :: Maybe Organ -> String
+processAndReport (Just organ) = report $ process organ
+processAndReport Nothing = "ERROR! ID not found."
+
+processRequest :: Int -> Map.Map Int Organ -> String 
+processRequest id catalogue = processAndReport organ
+  where
+    organ :: Maybe Organ
+    organ = Map.lookup id catalogue
+
+-- Q19.1
+emptyDrawers :: [Int] -> Map.Map Int Organ -> Int 
+emptyDrawers ids catalogue = length $ filter isNothing content
+  where
+    content :: [Maybe Organ]
+    content = getDrawerContents ids catalogue
+
+-- Q19.2
+
+maybeMap :: (a -> b) -> Maybe a -> Maybe b
+maybeMap _ Nothing = Nothing 
+maybeMap f (Just x) = Just (f x)
